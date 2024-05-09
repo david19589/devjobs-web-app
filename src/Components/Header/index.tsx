e61ccb4ddad3d0878213a9e5f7c9c34a840a6ef4
david19589 <davitnasyidashvili2006@gmail.com>
2024-05-09 22:46:50 +0400

    added tablet version of background img

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,5 +1,6 @@
 import Logo from "/src/assets/desktop/logo.svg";
 import HeaderBackground from "/src/assets/mobile/bg-pattern-header.svg";
+import HeaderBackgroundTablet from "/src/assets/desktop/bg-pattern-header.svg";
 import SunSvg from "/src/assets/desktop/icon-sun.svg";
 import MoonSvg from "/src/assets/desktop/icon-moon.svg";
 import Toggle from "../Toggle";
@@ -10,18 +11,29 @@ function Header(props: {
 }) {
   return (
     <div>
-      <div
-        className="flex justify-between items-center pt-[32px] pb-[72px] px-[24px] bg-cover"
-        style={{ backgroundImage: `url(${HeaderBackground})` }}
-      >
-        <img src={Logo} alt="Logo" />
-        <div className="flex items-center">
-          <img src={SunSvg} alt="SunSvg" />
-          <Toggle
-            isToggled={props.isToggled}
-            setIsToggled={props.setIsToggled}
-          />
-          <img src={MoonSvg} alt="MoonSvg" />
+      <div className="absolute w-full">
+        <img
+          className="w-full h-[136px] tablet:hidden"
+          src={HeaderBackground}
+          alt="HeaderBackground"
+        />
+        <img
+          className="w-full h-[160px] hidden tablet:flex"
+          src={HeaderBackgroundTablet}
+          alt="HeaderBackgroundTablet"
+        />
+      </div>
+      <div className="relative z-10 pt-[32px] pb-[72px] px-[24px]">
+        <div className="flex justify-between items-center">
+          <img src={Logo} alt="Logo" />
+          <div className="flex items-center">
+            <img src={SunSvg} alt="SunSvg" />
+            <Toggle
+              isToggled={props.isToggled}
+              setIsToggled={props.setIsToggled}
+            />
+            <img src={MoonSvg} alt="MoonSvg" />
+          </div>
         </div>
       </div>
     </div>
