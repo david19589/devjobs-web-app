216c44090f62d1b38413938947bb34d108d502cd
david19589 <davitnasyidashvili2006@gmail.com>
2024-05-12 21:59:02 +0400

    made a 'Home' component that displays job listings based on filtering criteria. It accepts props such as isToggled, searchValue, locationSearchValue, and isChecked to control the display of job listings. The component filters job listings based on company name and location, considering whether the job is full-time based on the isChecked prop. It dynamically updates the number of visible jobs using state (visibleJobs) and allows loading more jobs when clicking the 'Load More' button. Overall, this component provides a flexible and interactive interface for browsing job listings with filtering options.

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import Data from "../data.ts";
+
+function Home(props: {
+  isToggled: boolean;
+  searchValue: string;
+  locationSearchValue: string;
+  isChecked: boolean;
+}) {
+  const [visibleJobs, setVisibleJobs] = useState(6);
+
+  const filteredJobs = Data.filter((job) => {
+    const matchedSearch = job.company
+      .toLowerCase()
+      .includes(props.searchValue.toLowerCase());
+    const matchedLocation = job.location
+      .toLowerCase()
+      .includes(props.locationSearchValue.toLowerCase());
+    if (props.isChecked && job.contract.toLowerCase().includes("full time")) {
+      return matchedSearch && matchedLocation;
+    } else if (!props.isChecked && job.contract.toLowerCase().includes("")) {
+      return matchedSearch && matchedLocation;
+    }
+  });
+
+  return (
+    <>
+      <div className="flex flex-wrap justify-center max-w-[1440px] mr-auto ml-auto relative">
+        {filteredJobs.slice(0, visibleJobs).map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={`${
+                props.isToggled ? "bg-[#19202D]" : "bg-[#FFF]"
+              } flex cursor-pointer hover:shadow-md hover:shadow-gray-400 hover:scale-105 w-[336px] h-[272px] mb-[49px] mx-[24px] rounded-lg pr-[55px] pl-[32px] pb-[32px] group`}
+            >
+              <div>
+                <div
+                  className="w-[60px] h-[60px] translate-y-[-24px] flex justify-center items-center rounded-xl"
+                  style={{ backgroundColor: item.logoBackground }}
+                >
+                  <img
+                    className="px-[5px] py-[19px]"
+                    src={item.logo}
+                    alt={item.company}
+                  />
+                </div>
+                <div className="flex items-center mb-[16px] h-[17px]">
+                  <h2 className="font-[400] text-[16px] leading-[19.84px] text-[#6E8098]">
+                    {item.postedAt}
+                  </h2>
+                  <span className="flex h-[4px] w-[4px] mx-[12px] bg-[#6E8098] rounded-full self-end"></span>
+                  <h2 className="font-[400] text-[16px] leading-[19.84px] text-[#6E8098]">
+                    {item.contract}
+                  </h2>
+                </div>
+                <h1
+                  className={`${
+                    props.isToggled && "text-[#FFF]"
+                  } w-full font-[700] text-[20px] leading-[24.8px] mb-[17px] group group-hover:text-[#6E8098]`}
+                >
+                  {item.position}
+                </h1>
+                <h2 className="font-[400] text-[16px] leading-[19.84px] mb-[44px] text-[#6E8098]">
+                  {item.company}
+                </h2>
+                <h1 className="font-[700] text-[14px] leading-[17.36px] text-[#5964E0]">
+                  {item.location}
+                </h1>
+              </div>
+            </div>
+          );
+        })}
+        {visibleJobs < filteredJobs.length && (
+          <button
+            onClick={() => {
+              setVisibleJobs(visibleJobs + 3);
+            }}
+            className="px-[30px] py-[16px] mx-[30%] font-[700] text-[15px] leading-[19.84px] bg-[#5964E0] text-[#FFF] outline-none rounded mb-[61px]"
+          >
+            Load More
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
+export default Home;
