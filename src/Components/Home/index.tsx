48ab6591622dcec81d35f2a804437ba965f455d5
david19589 <davitnasyidashvili2006@gmail.com>
2024-05-14 14:42:44 +0400

    added some styles in 'Home' component

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -41,14 +41,14 @@ function Home(props: {
           setIsChecked={props.setIsChecked}
         />
       </div>
-      <div className="flex flex-wrap justify-center gap-[60px] max-w-[1440px] mr-auto ml-auto relative">
+      <div className="flex flex-wrap justify-center gap-[60px] max-w-[1440px] mr-auto ml-auto px-[24px] relative">
         {filteredJobs.slice(0, visibleJobs).map((item, index) => {
           return (
             <Link key={index} to={`/job/${item.id}`}>
               <div
                 className={`${
                   props.isToggled ? "bg-[#19202D]" : "bg-[#FFF]"
-                } flex cursor-pointer hover:shadow-md hover:shadow-gray-400 hover:scale-105 w-[336px] h-[272px] rounded-lg pr-[55px] pl-[32px] pb-[32px] group`}
+                } flex cursor-pointer hover:shadow-md hover:shadow-gray-400 hover:scale-105 w-full max-w-[336px] h-[272px] rounded-lg pr-[55px] pl-[32px] pb-[32px] group`}
               >
                 <div>
                   <div
